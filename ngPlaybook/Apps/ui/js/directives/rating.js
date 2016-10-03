003fe8acb3455e9d0ff0a5072b81773566accd10
Qasir Mehmood <qasirdev@gmail.com>
2016-10-03 23:10:34 +0100

    scope in link of directive

--- ngPlaybook/Apps/ui/js/directives/rating.js
+++ ngPlaybook/Apps/ui/js/directives/rating.js
@@ -1,11 +1,36 @@
 ﻿(function (module) {
 
-    var rating = function () {
+    var odfRating = function () {
         return {
-            
+            scope: {
+                value: "="
+            },
+            link: function (scope,element,attributes) {
+                var min = parseInt(element.min || "1");
+                var max = parseInt(element.max || "5");
+
+                scope.$watch("value", function (newValue) {
+                    element.empty();
+                    for (var i = 0; i < newValue; i++) {
+                        element.append('<button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-star" aria-hidden="true"></span></button>');
+                    }
+                });
+
+                element.on("click", function () {
+                    scope.$apply(function () {
+                        if (scope.value < max) {
+                            scope.value += 1;
+                        }
+                        else {
+                            scope.value = min;
+                        }
+                    });
+                });
+
+            }            
         };
     };
 
-    module.directive("rating", rating);
+    module.directive("odfRating", odfRating);
 
 }(angular.module("ui")));
