046316ce5890e7cd4b054039832017b6638a3204
Qasir Mehmood <qasirdev@gmail.com>
2016-10-04 13:06:28 +0100

    Form initial

---
+++ ngPlaybook/ngPlaybook/Apps/diagnostics/js/controllers/errorProneController.js
@@ -0,0 +1,23 @@
+﻿(function(module) {
+
+    var errorProneController = function(alerting) {
+        var model = this;
+
+        model.alertTypes = alerting.alertTypes;
+        model.alertMessage = "";
+        model.alertType = model.alertTypes[0];
+
+        model.createAlert = function() {
+            alerting.addAlert(model.alertType, model.alertMessage);
+            model.alertMessage = "";
+            model.alertType = model.alertTypes[0];
+        };
+
+        model.createException = function() {
+            throw new Error("Something has gone terribly wrong!");
+        };
+    };
+
+    module.controller("errorProneController", errorProneController);
+
+}(angular.module("diagnostics")))
