cd5645d0c9e289314436a209af1429efcad6f1a2
Qasir Mehmood <qasirdev@gmail.com>
2016-10-04 12:07:16 +0100

    working copy of UI

--- ngPlaybook/Apps/ui/js/directives/rating.js
+++ ngPlaybook/Apps/ui/js/directives/rating.js
@@ -5,32 +5,45 @@
             scope: {
                 value: "="
             },
-            link: function (scope,element,attributes) {
+            require: "odfRating",//odfRatingController
+            templateUrl: "/apps/ui/templates/rating.html",
+            controller: "odfRatingController",
+            link: function (scope,element,attributes,controller) { //its real job is link between scope and Dom
                 var min = parseInt(element.min || "1");
                 var max = parseInt(element.max || "5");
 
-                scope.$watch("value", function (newValue) {
-                    element.empty();
-                    for (var i = 0; i < newValue; i++) {
-                        element.append('<button class="btn btn-default btn-xs"><span class="glyphicon glyphicon-star" aria-hidden="true"></span></button>');
-                    }
-                });
-
-                element.on("click", function () {
-                    scope.$apply(function () {
-                        if (scope.value < max) {
-                            scope.value += 1;
-                        }
-                        else {
-                            scope.value = min;
-                        }
-                    });
-                });
-
+                controller.initialize(min,max);
             }            
         };
     };
 
+    var odfRatingController = function ($scope) { //here $scope is same as link: scope
+        this.initialize = function (min, max) {
+            $scope.preview = -1; //no star is selected
+            $scope.stars = new Array(max-min+1);
+        };
+
+        $scope.click = function ($index) {
+            $scope.value = $index + 1;
+        }
+        $scope.mouseover = function ($index) {
+            $scope.preview = $index;
+        };
+        $scope.mouseout = function ($index) {
+            $scope.preview = -1;
+        };
+        $scope.styles = function ($index) {
+            //styles directive return object and each property becomes class if it is true
+            return {
+                "glyphicon": true,
+                "glyphicon-star": $index < $scope.value,
+                "glyphicon-star-empty": $index >= $scope.value,
+                "starpreview": $index <= $scope.preview
+            };
+        };
+    };
+
+    module.controller("odfRatingController", odfRatingController);
     module.directive("odfRating", odfRating);
 
 }(angular.module("ui")));
