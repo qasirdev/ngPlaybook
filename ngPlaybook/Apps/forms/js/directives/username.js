8c92db3627a111077c1d11f2dc3bbc215980d904
Qasir Mehmood <qasirdev@gmail.com>
2016-10-05 20:39:12 +0100

    Final Running code

--- ngPlaybook/Apps/forms/js/directives/username.js
+++ ngPlaybook/Apps/forms/js/directives/username.js
@@ -17,13 +17,24 @@
                         .then(processResponse);
         };
 
+        var setupDom = function (input, form, scope) {
+            var inputName = input.attr("name");
+            var formName = form.$name;
+            var pending = "<div ng-if='" + formName + "." + inputName + ".$pending'>Checking name...</div>";
+            input.parent().append($compile(pending)(scope));
+        };
+
         return {
             restrict: "EA",
-            require: "ngModel", //this is Angular ng-model
-            link: function (scope, element, attributes, ngModel) {
+            require: ["ngModel", "^form"], //this is Angular ng-model and profileFormController
+            link: function (scope, element, attributes, controllers) {
+                var ngModel=controllers[0];
+                var formController=controllers[1];
                 //in startsWith.js
                 //ngModel.$validators.startsWith = function(modelValue){};
                 ngModel.$asyncValidators.username = validateUsername;
+
+                setupDom(element,formController,scope);
             }
 
         }
