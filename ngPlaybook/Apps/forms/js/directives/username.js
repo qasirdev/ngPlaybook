4dbcfac84cb2d743285a820d291997e50bc5b77d
Qasir Mehmood <qasirdev@gmail.com>
2016-10-05 20:26:08 +0100

    <div ng-show="profileform.username.$pending">

--- ngPlaybook/Apps/forms/js/directives/username.js
+++ ngPlaybook/Apps/forms/js/directives/username.js
@@ -2,11 +2,33 @@
 
     var username = function ($http, alerting, $q, $compile) {
 
+        var processResponse = function (response) {
+            if (response.data) {
+               return $q.when(true);
+            }
+            else {
+               return $q.reject(false);
+            }
+        };
+
+        var validateUsername = function (modelValue) {
+            //return promise, vlue from element
+            return $http.get("/api/namevalidation?name=" + encodeURI(modelValue))
+                        .then(processResponse);
+        };
+
         return {
+            restrict: "EA",
+            require: "ngModel", //this is Angular ng-model
+            link: function (scope, element, attributes, ngModel) {
+                //in startsWith.js
+                //ngModel.$validators.startsWith = function(modelValue){};
+                ngModel.$asyncValidators.username = validateUsername;
+            }
 
         }
     };
 
-    //module.directive("username", username);
+    module.directive("username", username);
 
 }(angular.module("forms")));
